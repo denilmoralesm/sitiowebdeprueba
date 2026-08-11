938587258dd00b24500b2fbe005f7c8fa4f0481e
Denil Digital Solutions <denildigitalsolutions@hotmail.com>
2026-08-10 23:44:58 -0400

    formulario POST en localhost

---
+++ php-practica/procesar.php
@@ -0,0 +1,8 @@
+<?php
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $mensaje = $_POST["mensaje"];
+    echo "<p>Nombre: $nombre </p>";
+    echo "<p>Correo: $correo </p>";
+    echo "<p>Mensaje: $mensaje </p>";
+?>
